c37a66a92dc3b3c35bca9502cd513c15a82db0ed
shaughnfinnerty <finnergizer@gmail.com>
2016-02-26 00:01:54 -0500

    Add sum-product computation, belief computation, iteration

--- decoder.py
+++ decoder.py
@@ -29,20 +29,62 @@ class Decoder:
     def prior_probability(self, z):
         p = lambda x: 1-1/(1 + math.exp((1-2*float(x))*4*float(z)/math.pow(self.noise, 2)))
         # p = lambda x: math.exp(-1*(math.pow((float(z)-1+2*float(x)), 2)/(2*self.variance)))/(self.noise*math.sqrt(2*math.pi))
-        return (p(0), p(1))
+        return [p(0), p(1)]
 
-    def compute(self):
-        return None
+    def compute(self, from_node, to_node, i):
+        msg = self.graph.edges[from_node.id + "-->" + to_node.id].messages[i]
+        if self.can_compute(from_node, to_node, i):
+            msg = [1.0, 1.0]
+            for edge in from_node.incoming_edges:
+                    if not edge.from_node.id == to_node.id:
+                        msg = [a*b for a,b in zip(msg, edge.messages[i])]
+            if from_node.is_factor_node:
+                # 0.5 is the normalized factor calculation (from 4)
+                # TODO: Use indicator factor function with Dirac delta to determine 0.5, 0.5 computationally
+                msg = [a*b for a,b in zip(msg, [0.5,0.5])]
+            else:
+                msg = [a*b for a,b in zip(msg, from_node.prior_prob)]
+        self.graph.edges[from_node.id + "-->" + to_node.id].messages.append(msg)
+
+
 
     def iterate(self, i):
-        return None
+        for id, node in self.graph.nodes.iteritems():
+            for edge in node.outgoing_edges:
+                self.compute(node, edge.to_node, i)
 
-    def can_compute(self, node, i):
-        for edge in node.edges:
-                return
+
+    def can_compute(self, from_node, to_node, i):
+        for edge in from_node.incoming_edges:
+            if edge.from_node.id != to_node.id and edge.messages[i] == None:
+                return False
         return True
+
+    def compute_belief(self):
+        results = []
+        for i in range(1,8):
+            belief = [1.0, 1.0]
+            node = self.graph.nodes["x"+str(i)]
+            for edge in node.incoming_edges:
+                belief = [a*b for a,b in zip(belief, edge.messages[-1])]
+            belief = [a*b for a,b in zip(belief, node.prior_prob)]
+            if belief[0] >= belief[1]:
+                results.append(0)
+            else:
+                results.append(1)
+        return results
+
+
+
 
 
 d = Decoder(1)
-d.initialize_decoder([-1,0,0,0,0,0,0])
+d.initialize_decoder([-0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5])
+d.iterate(0)
+print (d.compute_belief())
+d.iterate(1)
+print (d.compute_belief())
+d.iterate(2)
+print (d.compute_belief())
+
 
